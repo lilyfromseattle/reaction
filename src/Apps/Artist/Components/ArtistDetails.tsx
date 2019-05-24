513933122530e0859c7d55edff14a01998b958e2
Lily Pacegit <lilyfpace@gmail.com>
2019-05-24 13:05:40 -0400

    started

--- src/Apps/Artist/Components/ArtistDetails.tsx
+++ src/Apps/Artist/Components/ArtistDetails.tsx
@@ -1,10 +1,14 @@
 import { Flex, Sans, Spinner } from "@artsy/palette"
 import React from "react"
+import { useEffect, useState } from "react"
 
 export function ArtistDetails({ id }: { id: string }) {
   const [artist, { loading, error }] = useJson(
     `https://stagingapi.artsy.net/api/v1/artist/${id}`
   )
+
+  console.log("artist", artist)
+  console.log("error", error)
 
   if (loading) {
     return (
@@ -37,9 +41,43 @@ export function ArtistDetails({ id }: { id: string }) {
 function useJson(
   url: string
 ): [any, { loading: boolean; error: Error | null }] {
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState<Error | null>(null)
+
+  async function fetchUrl() {
+    const response = await fetch(url, {
+      headers: {
+        "X-Xapp-Token":
+          "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI1YmQ4Yjc4OTcxZDJkNjYzNTU2M2MzMjEiLCJzYWx0X2hhc2giOiI2ZmQ0Mjg1OGRmMTgyODNiMjdjNmIzNjY0OGRiODA1OSIsInJvbGVzIjoidXNlcixhZG1pbiIsInBhcnRuZXJfaWRzIjpbXSwiZXhwIjoxNTYzODE3OTA5LCJpYXQiOjE1NTg2MzM5MDksImF1ZCI6IjRlMzZlZmE0ZGI0ZTMyMDAwMTAwMDM1OSIsImlzcyI6IkdyYXZpdHkiLCJqdGkiOiI1Y2U2ZGRiNTA2MmQ4MTAwMTJkNjRiZWIifQ.vTELd4io8qDtG0FF0hswHf-ef_xV6xc0rcBefNhu1Zg",
+      },
+    })
+
+    try {
+      const json = await response.json()
+      console.log("JSON", json)
+
+      setData(json.data)
+      setError(null)
+    } catch (error) {
+      console.log("error", error)
+      setError(error)
+    }
+
+    setLoading(false)
+    // const json = await response.json()
+    // setData(json)
+    // setLoading(false)
+  }
+
+  useEffect(() => {
+    fetchUrl()
+  }, [])
+  return [data, { loading, error }]
+
   // TODO: implement me
-  return [
-    { name: "David", hometown: "Brighton, United Kingdom" },
-    { loading: false, error: null },
-  ]
+  // return [
+  //   { name: "David", hometown: "Brighton, United Kingdom" },
+  //   { loading: false, error: null },
+  // ]
 }
